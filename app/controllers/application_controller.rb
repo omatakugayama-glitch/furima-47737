edf4b123366542e3a349f1541c820cf018e6bac4
omatakugayama-glitch <omatakugayama@gmail.com>
2026-08-14 11:26:48 +0900

    ユーザー登録機能とバリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :family_name_kanji, :last_name_kanji, :family_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_kanji, :last_name_kanji, :family_name_kana, :last_name_kana, :birthday])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "furima#index"
+  root to: "items#index"
 end
