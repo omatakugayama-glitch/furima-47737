edd9f3ded3d676eda545a98d122a17e83649cca1
omatakugayama-glitch <omatakugayama@gmail.com>
2026-08-16 16:58:26 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,8 +20,9 @@ class ItemsController < ApplicationController
   end
 
   private
-  def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id, :price).merge(user_id: current_user.id)
-  end
 
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :condition_id, :prefecture_id, :shipping_fee_id,
+                                 :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
