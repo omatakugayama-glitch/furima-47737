b6c8ea5fdf1ba1c53829a919beeb93532398c7a5
omatakugayama-glitch <omatakugayama@gmail.com>
2026-08-16 15:14:17 +0900

    デバッグコード削除、テスト項目追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,6 @@ const price = () => {
 
   priceInput.addEventListener('input', () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById('add-tax-price');
     const tax = Math.floor(inputValue * 0.1);
